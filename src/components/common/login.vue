0ba0429c5bbe1f12ed20739ec3847720efbedf5e
pretty <wangxueqian@byrtsoft.com>
2019-08-20 16:45:05 +0800

    login

--- src/components/common/login.vue
+++ src/components/common/login.vue
@@ -1,21 +1,33 @@
 <template>
-    <div class="login">
-        <!-- <div>
-            <img src="./static/images/" alt="">
-        </div> -->
+    <div class="login" id="login">
+        <div class="login_logo">
+            <img src="static/images/logo.png" alt="">
+        </div>
         <div class="login_content">
             <div class="form_item">
                 <label for="username">用户名</label>
                 <input type="text" id="username">
             </div>
             <div class="form_item">
-                <label for="password">密&nbsp;码</label>
+                <label for="password">密&nbsp;&nbsp;&nbsp;码</label>
                 <input type="text" id="password">
             </div>
             <div class="form_item">
-                <label for="password">角&nbsp;色</label>
-                <input type="text" id="password">
+                <label>角&nbsp;&nbsp;&nbsp;色</label>
+                <el-select v-model="role" placeholder="请选择">
+                    <el-option
+                    v-for="item in options"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value">
+                    </el-option>
+                </el-select>
             </div>
+            <div class="form_remanber">
+                <img src="static/images/sure.png" alt="">
+                记住登录信息
+            </div>
+            <div class="form_btn"></div>
         </div>
         <p class="login_footer">
             2009-2019&nbsp;TJ-BYRT.com&nbsp;版权所有&nbsp;ICP证：京B2-2008010
@@ -26,52 +38,93 @@
 export default {
     data(){
         return {
-            dynamicValidateForm:{
-                userName:'',
-                passWord:'',
-                pro:'',
-            }
+            options: [
+                {value: '1',label: '物资处'},
+                {value: '2',label: '物资处2'}
+            ],
+            role:'1',
         }
     }
 }
 </script>
 <style lang="sass">
-.login{
+.el-select-dropdown.el-popper{
+    background: #093132;
+    border:1px solid #0f7079;
+}
+#login{
     width:100%;
     height:100%;
     background:url(../../../static/images/login_bg.png) no-repeat;
     background-size: 100% 100%;
     overflow:hidden;
+    .login_logo,.login_content{
+        width:35%;
+        box-sizing: border-box;
+        margin:0 auto;
+        min-width:550px;
+    }
+    .login_logo{
+        margin-top:80px;
+        margin-bottom:13px;
+        padding:0 20px;
+        img{
+            width:100%;
+            height:auto;
+        }
+    }
     .login_content{
-        width:40%;
+        padding:60px 104px 67px 104px;
         background:url(../../../static/images/login_content_bg.png) no-repeat;
-        background-size: 100% 100%;  
-        // margin
+        background-size: 100% 100%;
         .form_item{
-            // margin-bottom:28px;
+            margin-bottom:28px;
+            display:flex;
+            align-items:center;
+            padding:0 33px 0 20px;
             label{
                 font-size:18px;
                 color:#42f2e7;
+                margin-right:27px;
+                white-space: nowrap;
+            }
+            input{
+                font-size:16px;
+                background: #093132;
+                flex:1;
+                height:40px;
+                border:1px solid #0f7079;
+                padding:0 17px;
+                box-sizing: border-box;
+                color:#05dfd1;
+                border-radius: 4px;
+            }
+            .el-select{
+                width:100%;
+            }
+            input:-webkit-autofill,
+            textarea:-webkit-autofill,
+            select:-webkit-autofill {
+                -webkit-box-shadow: 0 0 0 1000px #093132 inset;
             }
         }
-        // margin:50px auto 0;
-        // .el-form-item {
-        //     margin-bottom: 28px;
-        //     .el-form-item__label{
-        //         font-size:18px;
-        //         color:#42f2e7;
-        //     }
-        //     .el-input__inner{
-        //         background: #093132;
-        //     }
-        //     .el-button.el-button--primary{
-        //         border:0;
-        //         height:60px;
-        //         width:100%;
-        //         background:url(../../../static/images/login_sure_btn.png) no-repeat;
-        //         background-size: 100% 100%;
-        //     }   
-        // }
+        .form_remanber{
+            font-size:16px;
+            color:#43c4d1;
+            img{
+                width:16px;
+                height:16px;
+                vertical-align: middle;
+                margin:0 18px 0 62px;
+            }
+        }
+        .form_btn{
+            margin-top:31px;
+            height:60px;
+            width:100%;
+            background:url(../../../static/images/login_sure_btn.png) no-repeat;
+            background-size: 100% 100%;
+        }
     }
     .login_footer{
         width:100%;
@@ -81,60 +134,4 @@ export default {
         color:#0e8a7d;
     }
 }
-
-// .logo{
-//     margin-left:calc(100vw/2 - 29px);
-//     width: 58px;
-//     height: 58px;
-//     line-height: 58px;
-//     text-align: center;
-//     border: 1px solid rgba(187, 187, 187, 1);
-//     border-radius:29px;
-//     color:#fff;
-// }
-
-// .system{
-//     color : #fff;
-//     left: 609px;
-//     font-size: 24px;
-//     font-family: SourceHanSansSC-regular;
-//     margin-top:50px;
-// }
-
-// .login_box{
-//     margin-top:50px;
-//     width: 330px;
-//     height: 100px;
-//     border-radius: 10px;
-//     text-align: center;
-//     border: 1px solid rgba(187, 187, 187, 1);
-//     margin-left:calc(100vw/2 - 165px);
-//     padding : 0 5px;
-//     background:#fff;
-// }
-// .login_box input{
-//     width:100%;
-//     border:none;
-//     text-algin:center;
-//     height:50px;
-//     color:#A4A4A4;
-// }
-// .login_box input:first-child{
-//     border-bottom:1px solid #bbbbbb;
-// }
-
-// .login_btn{
-//     margin-top: 10px;
-//     width: 340px;
-//     height: 50px;
-//     line-height: 50px;
-//     border-radius: 10px;
-//     background-color: #44acc8;
-//     text-align: center;
-//     margin-left: calc(100vw/2 - 165px);
-//     border: 2px solid white;
-//     color: #fff;
-//     font-size: 16px;
-//     cursor:pointer;
-// }
 </style>
