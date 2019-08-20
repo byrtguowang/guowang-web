516d3f65bfd618f05780c7e899211b0d945f4409
pretty <wangxueqian@byrtsoft.com>
2019-08-20 15:02:49 +0800

    login

--- src/components/common/login.vue
+++ src/components/common/login.vue
@@ -1,138 +1,132 @@
 <template>
-    <div class="login">
-        <div class="logo">LOGO</div>
-        <div class="system">xxx项目</div>
-        <div class="login_box">
-            <input  type="text" placeholder="用户名" v-model="userN"/>
-            <input  type="password" placeholder="密码" v-model="passW"/>
+    <div class="login" id="login">
+        <!-- <div>
+            <img src="./static/images/" alt="">
+        </div> -->
+        <div class="login_content">
+            <div>
+                <label for="username">用户名</label>
+                <input type="text" id="username">
+            </div>
+            <div>
+                <label for="password">密&nbsp;码</label>
+                <input type="text" id="password">
+            </div>
+            <div>
+                <label for="password">角&nbsp;色</label>
+                <input type="text" id="password">
+            </div>
         </div>
-        <div class="login_btn" @click="login">登陆</div>
+        <p class="login_footer">
+            2009-2019&nbsp;TJ-BYRT.com&nbsp;版权所有&nbsp;ICP证：京B2-2008010
+        </p>
     </div>
 </template>
-
 <script>
-import { 
-    login, // 登录
-} from '@api/ns_api/index.js'
-import { setStorage } from "@common/mUtils.js";
 export default {
-    name: "login",
     data(){
-        return{
-            userN : 'admin',
-            passW : '123456'
-        }
-    },
-    methods:{
-        // 登录
-        login() {
-            if (this.userN != ''){
-                if (this.passW != ''){
-
-                    this.$router.push({
-                        path : '/Home'
-                    })
-
-                    // 请求接口演示
-
-                    // login({
-                    //     username:this.userN,
-                    //     password:this.passW
-                    // })
-                    // .then(res =>{
-                    //     if (res.data.status == 0){
-                    //         setStorage('userId',res.data.data.id)
-                    //         setStorage('name', res.data.data.name)
-                    //         setStorage('loginName', res.data.data.loginName)
-
-                    //         this.$router.push({
-                    //             path : '/Home'
-                    //         })
-                    //     } else {
-                    //         this.$message({
-                    //             message:res.data.message,
-                    //             type: 'error'
-                    //         });
-                    //     }
-                    // })
-                } else {
-                    this.$message({
-                        message:'请输入密码',
-                        type: 'error'
-                    });
-                }
-            } else {
-                this.$message({
-                    message:'请输入用户名',
-                    type: 'error'
-                });
+        return {
+            dynamicValidateForm:{
+                userName:'',
+                passWord:'',
+                pro:'',
             }
         }
-    },
+    }
 }
 </script>
-
-<style lang="sass" scoped>
-.login{
-    background:#44ACC8;
-    text-align:center;
-    padding-top:130px;
-    height:calc(100vh - 130px);
-}
-
-.logo{
-    margin-left:calc(100vw/2 - 29px);
-    width: 58px;
-    height: 58px;
-    line-height: 58px;
-    text-align: center;
-    border: 1px solid rgba(187, 187, 187, 1);
-    border-radius:29px;
-    color:#fff;
-}
-
-.system{
-    color : #fff;
-    left: 609px;
-    font-size: 24px;
-    font-family: SourceHanSansSC-regular;
-    margin-top:50px;
-}
-
-.login_box{
-    margin-top:50px;
-    width: 330px;
-    height: 100px;
-    border-radius: 10px;
-    text-align: center;
-    border: 1px solid rgba(187, 187, 187, 1);
-    margin-left:calc(100vw/2 - 165px);
-    padding : 0 5px;
-    background:#fff;
-}
-.login_box input{
+<style lang="sass">
+#login{
     width:100%;
-    border:none;
-    text-algin:center;
-    height:50px;
-    color:#A4A4A4;
-}
-.login_box input:first-child{
-    border-bottom:1px solid #bbbbbb;
+    height:100%;
+    background:url(../../../static/images/login_bg.png) no-repeat;
+    background-size: 100% 100%;
+    overflow:hidden;
+    .demo-dynamic{
+        width:40%;
+        background:url(../../../static/images/login_content_bg.png) no-repeat;
+        background-size: 100% 100%;  
+        margin:50px auto 0;
+        .el-form-item {
+            margin-bottom: 28px;
+            .el-form-item__label{
+                font-size:18px;
+                color:#42f2e7;
+            }
+            .el-input__inner{
+                background: #093132;
+            }
+            .el-button.el-button--primary{
+                border:0;
+                height:60px;
+                width:100%;
+                background:url(../../../static/images/login_sure_btn.png) no-repeat;
+                background-size: 100% 100%;
+            }   
+        }
+    }
+    .login_footer{
+        width:100%;
+        position:fixed;
+        bottom:34px;
+        text-align: center;
+        color:#0e8a7d;
+    }
 }
 
-.login_btn{
-    margin-top: 10px;
-    width: 340px;
-    height: 50px;
-    line-height: 50px;
-    border-radius: 10px;
-    background-color: #44acc8;
-    text-align: center;
-    margin-left: calc(100vw/2 - 165px);
-    border: 2px solid white;
-    color: #fff;
-    font-size: 16px;
-    cursor:pointer;
-}
+// .logo{
+//     margin-left:calc(100vw/2 - 29px);
+//     width: 58px;
+//     height: 58px;
+//     line-height: 58px;
+//     text-align: center;
+//     border: 1px solid rgba(187, 187, 187, 1);
+//     border-radius:29px;
+//     color:#fff;
+// }
+
+// .system{
+//     color : #fff;
+//     left: 609px;
+//     font-size: 24px;
+//     font-family: SourceHanSansSC-regular;
+//     margin-top:50px;
+// }
+
+// .login_box{
+//     margin-top:50px;
+//     width: 330px;
+//     height: 100px;
+//     border-radius: 10px;
+//     text-align: center;
+//     border: 1px solid rgba(187, 187, 187, 1);
+//     margin-left:calc(100vw/2 - 165px);
+//     padding : 0 5px;
+//     background:#fff;
+// }
+// .login_box input{
+//     width:100%;
+//     border:none;
+//     text-algin:center;
+//     height:50px;
+//     color:#A4A4A4;
+// }
+// .login_box input:first-child{
+//     border-bottom:1px solid #bbbbbb;
+// }
+
+// .login_btn{
+//     margin-top: 10px;
+//     width: 340px;
+//     height: 50px;
+//     line-height: 50px;
+//     border-radius: 10px;
+//     background-color: #44acc8;
+//     text-align: center;
+//     margin-left: calc(100vw/2 - 165px);
+//     border: 2px solid white;
+//     color: #fff;
+//     font-size: 16px;
+//     cursor:pointer;
+// }
 </style>
