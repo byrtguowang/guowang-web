43d33e1236b0d752e9e0ba3a361cb339ebe90172
pretty <wangxueqian@byrtsoft.com>
2019-08-26 15:01:51 +0800

    'dd'

--- src/components/page/salesOrderInformation/index.vue
+++ src/components/page/salesOrderInformation/index.vue
@@ -61,8 +61,7 @@
             <div class="table">
                 <el-table
                     :data="tableData"
-                    style="width: 100%"
-                    height="450">
+                    style="width: 100%">
                     <el-table-column
                     prop="sgPurchaseorder"
                     align="center"
@@ -105,6 +104,11 @@
                         </template>
                     </el-table-column>
                 </el-table>
+                <el-pagination
+                    small
+                    layout="prev, pager, next"
+                    :total="5000">
+                </el-pagination>
             </div>
         </div>
      </div>
@@ -181,148 +185,10 @@
             </ul>
         </div>
      </div>
-    <!-- <div class="information_top mb10">
-        <div class="left">
-            <div class="title">
-                <p>供应商信息</p>
-            </div>
-            <div class="information_content echarts_wrap">
-                <div class="echarts_box">
-                    <div class="echarts_tit">
-                        <p>供应商名称</p>
-                        <span class="ellipsis cursor">{{supplierName}}</span>
-                    </div>
-                    <div class="echarts_con echarts_con1" ref="echarts1"></div>
-                    <div class="echarts_con echarts_con2" ref="echarts2"></div>
-                </div>
-                <div class="echarts_box">
-                    <div class="echarts_tit">
-                        <p>供应商地址</p>
-                        <span class="ellipsis cursor" :title="supplierAddr">{{supplierAddr}}</span>
-                    </div>
-                    <div class="echarts_con echarts_con3" ref="echarts3"></div>
-                    <div class="echarts_con echarts_con4" ref="echarts4"></div>
-                </div>
-            </div>
-        </div>
-        <div class="right">
-            <div class="title">
-                <p>工艺流程</p>
-            </div>
-            <div class="right_box">
-                <div class="item">
-                    <img src="static/images/img.png" >
-                    <div class="gj_name">PCB板贴片检测</div>
-                    <div class="gj_btn cursor" @click="jump2('')">{{processList['PCB板贴片检测']||0}}项告警</div>
-                </div>
-                <div class="item">
-                    <img src="static/images/img.png" >
-                    <div class="gj_name">单板测试</div>
-                    <div class="gj_btn cursor" @click="jump2('D_VeneerTest_DNB')">{{processList['单板测试']||0}}项告警</div>
-                </div>
-                <div class="item">
-                    <img src="static/images/img.png" >
-                    <div class="gj_name">电流测试</div>
-                    <div class="gj_btn cursor" @click="jump2('')">{{processList['电流测试']||0}}项告警</div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="information_bottom">
-        <div class="left">
-            <div class="title">
-                <p>销售订单信息 <span class="cursor" @click="getMore">更多></span></p>
-            </div>
-            <div class="information_content table">
-                <el-table
-                    :data="tableData"
-                    style="width: 100%"
-                    height="310">
-                    <el-table-column
-                    prop="sgPurchaseorder"
-                    align="center"
-                    label="国网采购合同号">
-                    </el-table-column>
-                    <el-table-column
-                    prop="salesOrderCode"
-                    align="center"
-                    label="销售订单号">
-                    </el-table-column>
-                    <el-table-column
-                    prop="materialsNum"
-                    align="center"
-                    width="100"
-                    label="订单数量">
-                    </el-table-column>
-                    <el-table-column
-                    prop="sgProjectname"
-                    align="center"
-                    label="工程项目名称">
-                    </el-table-column>
-                    <el-table-column
-                    prop="deliveryDate"
-                    align="center"
-                    width="100"
-                    label="交货日期">
-                    </el-table-column>
-                    <el-table-column
-                    align="center"
-                    label="进度">
-                        <template slot-scope="scope">
-                            <el-progress :percentage="parseFloat(scope.row.percent||0)"></el-progress>
-                        </template>
-                    </el-table-column>
-                </el-table>
-            </div>
-        </div>
-        <div class="right">
-            <div class="title">
-                <p>实验数据</p>
-            </div>
-            <div class="right_box scroll_h">
-                <div class="item">
-                    <img src="static/images/img.png" >
-                    <div class="gj_name">耐压试验</div>
-                    <div class="gj_btn cursor" @click="jump2('D_Pressure_DNB')">{{processList['耐压试验']||0}}项告警</div>
-                </div>
-                <div class="item">
-                    <img src="static/images/img.png" >
-                    <div class="gj_name">基本误差试验</div>
-                    <div class="gj_btn cursor" @click="jump2('D_BasicError_DNB')">{{processList['基本误差试验']||0}}项告警</div>
-                </div>
-                <div class="item">
-                    <img src="static/images/img.png" >
-                    <div class="gj_name">日计时误差试验</div>
-                    <div class="gj_btn cursor" @click="jump2('D_TimingError_DNB')">{{processList['日计时误差试验']||0}}项告警</div>
-                </div>
-                <div class="item">
-                    <img src="static/images/img.png" >
-                    <div class="gj_name">参数设置</div>
-                    <div class="gj_btn cursor" @click="jump2('D_Parameter_DNB')">{{processList['参数设置']||0}}项告警</div>
-                </div>
-                <div class="item">
-                    <img src="static/images/img.png" >
-                    <div class="gj_name">通信端口检验</div>
-                    <div class="gj_btn cursor" @click="jump2('')">{{processList['通信端口检验']||0}}项告警</div>
-                </div>
-            </div>
-        </div>
-    </div> -->
 </div>
 </template>
 
 <script>
-import {
-    ringBrightGreen, //当前生产状态
-    ringDarkGreen, //当前生产状态
-    rightPieChart,//今日报警信息
-    getProductionProcess,//三维生产工艺
-    salesOrderInfo
-} from '@api/supplierInformation'
-import {
-    getOrderCompletionRate, //订单完成率
-    getQualityControl, //生产质量监控
-} from '@api/map'
 export default {
     data() {
         return {
@@ -335,132 +201,15 @@ export default {
                 {},
                 {},
                 {},
+                {},
+                {},
             ],//表格数据
         }
     },
     mounted() {
-        // this.salesOrderInfo();
     },
     
-    methods: { 
-        // // 当前生产状态
-        // async ringGreen(){
-        //     let {data:data1}=await ringBrightGreen({});
-        //     let {data:data2}=await ringDarkGreen({});
-        //     if(data1.status===0||data1.status==='0'){
-        //         this.echarts1Date.push({
-        //             name:'实际下线（台）',
-        //             value:parseFloat(data1.data),
-        //         })
-        //     }else{
-        //         this.$message({
-        //             type:'error',
-        //             message:data1.message
-        //         });
-        //         this.echarts1Date.push({
-        //             name:'实际下线（台）',
-        //             value:0,
-        //         });
-        //     } 
-        //     if(data2.status===0||data2.status==='0'){
-        //         this.echarts1Date.push({
-        //             name:'当前计划（台）',
-        //             value:parseFloat(data2.data),
-        //         })
-        //     }else{
-        //         this.$message({
-        //             type:'error',
-        //             message:data1.message
-        //         });
-        //         this.echarts1Date.push({
-        //             name:'当前计划（台）',
-        //             value:0,
-        //         });
-        //     }
-        //     this.drawLine1();
-        // },
-        // //今日报警信息
-        // async rightPieChart(){
-        //     let {data}=await rightPieChart({});
-        //     if(data.status===0||data.status==='0'){
-        //         const obj={
-        //             D_BasicError_DNB:'电能误差',
-        //             D_Parameter_DNB:'电能参数',
-        //             D_Pressure_DNB:'电能耐压',
-        //             D_TimingError_DNB:'电能日计时',
-        //             D_VeneerTest_DNB:'电能表单板'
-        //         }
-        //         for(let item of data.data){
-        //             item.name=obj[item.dataName]
-        //         }
-        //         this.echarts3Date=data.data?data.data:[];
-        //     }
-        //     else{
-        //         this.echarts3Date=[];
-        //         this.$message({
-        //             type:'error',
-        //             message:data.message
-        //         });
-        //     }
-        //     this.drawLine3();
-        // },
-        // //订单完成率
-        // async getOrderCompletionRate(){
-        //     const {data}=await getOrderCompletionRate({});
-        //     if(data.status===0||data.status==='0') this.echarts2Date=data.data?data.data:[];
-        //     else{
-        //         this.echarts2Date=[];
-        //         this.$message({
-        //             type:'error',
-        //             message:data.message
-        //         });
-        //     }
-        //     this.drawLine2();
-        // },
-        // //生产质量监控
-        // async getQualityControl(){
-        //     const {data}=await getQualityControl({});
-        //     if(data.status===0||data.status==='0') this.echarts4Date=data.data?data.data:[];
-        //     else{
-        //         this.echarts4Date=[];
-        //         this.$message({
-        //             type:'error',
-        //             message:data.message
-        //         });
-        //     }
-        //     this.drawLine4();
-        // },
-        // // 表格数据
-        // async salesOrderInfo(){
-        //     let {data}=await salesOrderInfo({});
-        //     if(data.status===0||data.status==='0'){
-        //         this.tableData=data.data?data.data:[];
-        //     }else{
-        //         this.tableData=[];
-        //         this.$message({
-        //             type:'error',
-        //             message:data.message
-        //         })
-        //     }
-        // }, 
-        // //三维生产工艺
-        // async getProductionProcess(){
-        //     const {data}=await getProductionProcess({
-        //         supplierID:sessionStorage.getItem('supplierID')
-        //     });
-        //     if(data.status===0||data.status==='0'){
-        //         this.processList=data.data?data.data:{};
-        //     }else{
-        //         this.processList={};
-        //         this.$message({
-        //             type:'error',
-        //             message:data.message
-        //         })
-        //     }
-        // },
-        // // 更多
-        // getMore(){
-        // },
+    methods: {
     },
 };
 </script>
@@ -490,6 +239,15 @@ export default {
                 }
             }
         }
+        .el-pagination.el-pagination--small{
+            &,* {
+                background:transparent;
+                color:#fff;   
+            }
+            .active{
+                color: #409EFF;
+            }
+        }
         .el-table{
             color:#23e4c7;
             thead{
@@ -509,6 +267,7 @@ export default {
             }
             .el-table__body-wrapper.is-scrolling-none{
                 background:rgba(27,35,44,.4);
+                padding-bottom:40px;
             }
             .el-table__body-wrapper.is-scrolling-none,.el-table__body{
                 position:relative;
@@ -616,7 +375,16 @@ export default {
                 }
             }
         }
-        
+        .table{
+            padding-bottom:18px;
+            position:relative;
+            .el-pagination.el-pagination--small{
+                position:absolute;
+                bottom:25px;
+                right:50%;
+                transform:translateX(50%);
+            }
+        }
     }
     .top_box ,.botttom_box{
         background:linear-gradient(#02514c, #012a2f);
@@ -650,146 +418,4 @@ export default {
         }
     }
 }
-// .supplier_information{
-//     background-color:#15181f;
-//     color:#fff;
-//     // title样式
-//     .title{
-//         height:37px;
-//         border-left:10px solid #22c4ac;
-//         background:linear-gradient(to right, rgba(13,99,119,0.41), rgba(34,196,172,0.41));
-//         margin-bottom:11px;
-//         p{
-//             line-height:37px;
-//             color:#27F0D2;
-//             font-size:18px;
-//             margin-left:20px;
-//             position:relative;
-//             span{
-//                 position:absolute;
-//                 right:30px;
-//                 font-size:14px;
-//                 color:#20ad98;
-//             }
-//         }
-//     }
-//     .information_top,.information_bottom{
-//         display:flex;
-//     }
-//     .information_content{
-//         background:linear-gradient(#02514c, #012a2f);
-//         flex:1;
-//         &.table{
-//             padding:44px 20px 10px;
-//         }
-//         &.echarts_wrap{
-//             padding:12px 17px 0 21px;
-//             display:flex;
-//             .echarts_box{
-//                 flex:1;
-//                 display:flex;
-//                 flex-direction:column;
-//                 &:first-child{
-//                     margin-right:41px;
-//                 }
-//                 .echarts_tit{
-//                     background:linear-gradient(to right, #035853, #139E8E, #035853);
-//                     position:relative;
-//                     font-size:16px;
-//                     p{
-//                         width:156px;
-//                         height:41px;
-//                         line-height:41px;
-//                         background:#1b232c;
-//                         border:1px solid #34b6a2;
-//                         text-align:center;
-                        
-//                     }
-//                     span{
-//                         position:absolute;
-//                         width:50%;
-//                         text-align:right;
-//                         right:30px;
-//                         top:50%;
-//                         transform: translateY(-50%);
-                        
-//                     }
-//                 }
-//                 .echarts_con{
-//                     flex:1;
-//                     position:relative;
-//                 }
-//                 .echarts_con:before{
-//                     content:'当前生产状态';
-//                     position:absolute;
-//                     top:11px;
-//                     left:0;
-//                     font-size:16px;
-//                     font-weight:bold;
-//                 }
-//                 .echarts_con2:before{
-//                      content:'订单完成率';
-//                 }
-//                 .echarts_con3:before{
-//                      content:'今日报警信息';
-//                 }
-//                 .echarts_con4:before{
-//                      content:'生产质量监控';
-//                 }
-//             }
-//         }
-//     }
-//     //左右布局的样式
-//     .left{
-//         flex:3;
-//         margin-right:11px;
-//         display:flex;
-//         flex-direction:column;
-//     }
-//     .right{
-//         flex:1;
-//         .right_box{
-//             background:linear-gradient(#02514c, #012a2f);
-//             padding:12px;
-//             display:flex;
-//             flex-wrap:wrap;
-//             .item{
-//                 width:49%;
-//                 padding-right:1%;
-//                 margin-bottom:10px;
-                
-//                 img{
-//                     height:128px;
-//                     width:100%;
-//                 }
-//                 .gj_name{
-//                     height:31px;
-//                     line-height:31px;
-//                     text-align:center;
-//                     color:#dcdcdc;
-//                     border:1px solid #f59a1b;
-//                     background:#011c1c;
-//                     font-weight:bold;
-//                     font-size:16px;
-//                 }
-//                 .gj_btn{
-//                     height:33px;
-//                     line-height:33px;
-//                     text-align:center;
-//                     border-left:3px solid #f59a1b;
-//                     border-right:3px solid #f59a1b;
-//                     background:linear-gradient(to right,rgba(141,69,0,0.57),rgba(171,128,40,0.57),rgba(141,69,0,0.57));
-//                     font-size:16px;
-//                 }
-//             }
-//         }
-//         .mb10{
-//             margin-bottom:10px;
-//         }
-//         .scroll_h{
-//             height:340px;
-//             overflow-y: scroll;
-//         }
-//     }
-// }
 </style>
